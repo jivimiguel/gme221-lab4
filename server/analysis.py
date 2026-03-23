d98023381dbe58a76acfc36fb280a4c11ed3c8eb
jivimiguel <jgpucio@up.edu.ph>
2026-03-24 00:52:44 +0800

    Implemented spatial weights module and neighnorhood structures

--- server/analysis.py
+++ server/analysis.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 from sqlalchemy import create_engine
+from spatial_weights import contiguity_weights, knn_weights, distance_weights
 
 # Database credentials
 host = "localhost"
@@ -21,3 +22,7 @@ gdf = gpd.read_postgis(sql_query, engine, geom_col="geom")
 
 print(gdf.head())
 print("CRS:", gdf.crs)
+
+w = contiguity_weights(gdf)
+
+print("Neighbors:" , w.neighbors)
